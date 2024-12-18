0cde8e19f716afdd43905864c05a7de5fac8d3f5
Dante-Nephilim <sakkara.ps@gmail.com>
2024-12-18 21:06:14 +0530

    setup angular client & remove extra import in server/index.js file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import userRouter from "./routes/userRoutes.js";
 import connectDB from "./config/db.js";
 import userRouter from "./routes/userRoutes.js";
 import contractRouter from "./routes/contractorRoutes.js";
